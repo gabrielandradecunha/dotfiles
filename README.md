caa49a2934e600a6e085706082cb86be960ce08b
Gabriel Andrade Cunha <gandradecortez50@gmail.com>
2025-01-11 14:40:38 -0400

    Add readme.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# no place like ~/
+
+This repository is a collection of my personal dotfiles and scripts, designed to set up a productive development environment on Linux.
+
+Feel free to use.
--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 export PATH=$PATH:/home/gabriel/dotfiles/bin:/home/gabriel/dotfiles/bin/install
 
 echo "export PATH=$PATH:/home/gabriel/dotfiles/bin:/home/gabriel/dotfiles/bin/install" >> ~/.zshrc
+echo "export PATH=$PATH:/home/gabriel/dotfiles/bin:/home/gabriel/dotfiles/bin/install" >> ~/.bashrc
 
 # or ln -s
 cp -R * ~/
